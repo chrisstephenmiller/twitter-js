7be4a3991a77b02c8ce7e69911206a1ade2bacfe
chrisstpehenmiller <csmiller4567@gmail.com>
2018-03-05 19:09:09 -0600

    adjusting app.js

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const app = express();
 const morgan = require('morgan');
 const nunjucks = require('nunjucks')
 
-const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-
 app.listen(3000, () => console.log('Server listening!'))
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', { noCache: true }); // point nunjucks to the proper directory for templates
+
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
 
 app.get('/one/', function (req, res, next) {
     res.render('index.html', {title: 'Hall of Fame', people: people});
 })
 
-app.set('view engine', 'html'); // have res.render work with html files
-app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
-nunjucks.configure('views', { noCache: true }); // point nunjucks to the proper directory for templates
+
