85cac727016c39e9fe9b342052576866d603cba1
David Fudacz <davidfudacz@gmail.com>
2018-03-06 10:21:31 -0600

    data storage and first router module

--- app.js
+++ app.js
@@ -2,17 +2,12 @@ const express = require('express')
 const app = express();
 const morgan = require('morgan');
 const nunjucks = require('nunjucks')
+const routes = require('./routes');
 
 app.listen(3000, () => console.log('Server listening!'))
 
+app.use('/',routes);
+app.use(express.static('public'));
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 nunjucks.configure('views', { noCache: true }); // point nunjucks to the proper directory for templates
-
-const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-
-app.get('/one/', function (req, res, next) {
-    res.render('index.html', {title: 'Hall of Fame', people: people});
-})
-
-
