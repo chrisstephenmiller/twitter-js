5dd0491d859d9414e39106f8e06a98e64771aafd
David Fudacz <davidfudacz@gmail.com>
2018-03-06 13:40:24 -0600

    fixed static link issue

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const morgan = require('morgan');
 const nunjucks = require('nunjucks')
 const routes = require('./routes');
 const bodyParser = require('body-parser')
+const server = app.listen(3000, () => console.log('Server listening!'))
+const socketio = require('socket.io');
+const io = socketio.listen(server);
 
-app.listen(3000, () => console.log('Server listening!'))
 
-app.use('/',routes);
+app.use('/',routes(io));
 app.use(express.static('public'));
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
--- routes/index.js
+++ routes/index.js
@@ -4,35 +4,36 @@ const router = express.Router();
 const tweetBank = require('../tweetBank');
 const bodyParser = require('body-parser')
 
-router.use(bodyParser.urlencoded({ extended: false }))
 
-router.get('/', function (req, res) {
-  let tweets = tweetBank.list();
-  res.render('index', { tweets: tweets, showForm: true });
-});
+module.exports = function (io) {
+  io.sockets.emit('newTweet', { tweet: 'here I AM!' });
 
-router.post('/tweets', function(req, res) {
-  let name = req.body.name;
-  let text = req.body.text;
-  tweetBank.add(name, text);
-  res.redirect('back');
-});
+  router.use(bodyParser.urlencoded({ extended: false }))
+  
+  router.get('/', function (req, res) {
+    let tweets = tweetBank.list();
+    res.render('index', { tweets: tweets, showForm: true });
+  });
+  
+  router.post('/tweets', function(req, res) {
+    let name = req.body.name;
+    let text = req.body.text;
+    tweetBank.add(name, text);
+    res.redirect('back');
+  });
+  
+  router.get('/users/:name', function (req, res) {
+    let username = req.params.name;
+    let list = tweetBank.find({ name: username });
+    res.render('index', { tweets: list, showForm: true, name: username })
+  });
+  
+  router.get('/tweets/:id', function (req, res) {
+    let tweetId = req.params.id;
+    let list = tweetBank.find({ id: +tweetId });
+    res.render('index', { tweets: list })
+  });
 
-router.get('/:fileName', function (req, res) {
-  let fileName = req.params.fileName;
-  res.sendFile(fileName);
-});
 
-router.get('/users/:name', function (req, res) {
-  let username = req.params.name;
-  let list = tweetBank.find({ name: username });
-  res.render('index', { tweets: list, showForm: true, name: username })
-});
-
-router.get('/tweets/:id', function (req, res) {
-  let tweetId = req.params.id;
-  let list = tweetBank.find({ id: +tweetId });
-  res.render('index', { tweets: list })
-});
-
-module.exports = router;
+  return router;
+};
