03f92c198d337cdc712c34a07615043b76d1040c
chrisstpehenmiller <chrismiller@cm.local>
2018-03-05 19:07:10 -0600

    nunjucks template

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express();
+const morgan = require('morgan');
+const nunjucks = require('nunjucks')
+
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
 
 app.listen(3000, () => console.log('Server listening!'))
 
-app.get('/is-anybody-in-there/', function (req, res, next) {
-    res.send(req.method + ' ' + req.path);
+app.get('/one/', function (req, res, next) {
+    res.render('index.html', {title: 'Hall of Fame', people: people});
 })
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', { noCache: true }); // point nunjucks to the proper directory for templates
